254eb921244858b6f5740452cb09fd9d8a6258fa
Kashif Saeed <mks786@gmail.com>
2020-05-07 12:36:21 +0500

    RabbitMQ sample

---
+++ RabbitMQ/Sample/RabbitMQSample/Producer/Sender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "Getting started with .net core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("sent message {0}...", message);
+            }
+            Console.WriteLine("Press [enter] to exit the Sender App");
+            Console.ReadLine();
+        }
+    }
+}
