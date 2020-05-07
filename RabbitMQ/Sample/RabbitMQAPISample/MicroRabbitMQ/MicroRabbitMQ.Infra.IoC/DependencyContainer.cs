bd85da6275bc06653506dc2881428179785f3f6a
Kashif Saeed <mks786@gmail.com>
2020-05-08 03:46:08 +0500

    IoC

---
+++ RabbitMQ/Sample/RabbitMQAPISample/MicroRabbitMQ/MicroRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbitMQ.Domain.Core.Bus;
+using MicroRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
